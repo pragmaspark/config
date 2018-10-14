d84835d79a1d7d8ae6f3fe866cf78e92f6b4dae6
pragmaspark <konst.toshchev@gmail.com>
2018-10-14 16:46:38 +0300

    1

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Install bash configs
+exec cp ./bash/bashrs ~/.bashrc
+
 
 # Install fish configs
+if ! [ -d ~/.config/fish/ ]; then
+        mkdir -p ~/.config/fish
+fi
+
+exec cp ./fish/* ~/.config/fish/
+
 
 # Install sway configs
 
-# Install nvim configs
 
+# Install nvim configs
 if ! [ -d ~/.config/nvim/ ]; then
 	mkdir -p ~/.config/nvim
 fi
